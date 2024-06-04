0d4ba70e20c512a65ea5631af5e55145c321a842
salashiram <hiramsalas@192.168.1.21>
2024-06-04 02:23:19 -0600

    login y registro design

--- app/src/main/java/com/example/app1/ui/MainActivity.kt
+++ app/src/main/java/com/example/app1/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.app1.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.app1.Pantalla2
+import android.widget.TextView
 import com.example.app1.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvLogin: TextView = findViewById(R.id.linkText)
+
+        tvLogin.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla2::class.java)
+            startActivity(intent)
+        }
     }
 }
